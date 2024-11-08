1b9acdd19cdba5eab5bb774f65908b559dc7a0ff
mpotulom28 <mpotulom28@gmail.com>
2024-11-08 02:00:21 +0200

    Add UnderDev component with countdown timer and update styles

--- src/app/components/ImageModal/page.tsx
+++ src/app/components/ImageModal/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import useGlobalContext from "../../context/GlobalContext";
-import styles from "./img-modal.module.css";
+import styles from "./page.module.css";
 import { FaRegTimesCircle } from "react-icons/fa";
 
 const ImageModal: React.FC = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import UnderDev from "./components/UnderDev/page";
 import styles from "./page.module.css";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
-			<main className={styles.main}>Main</main>
+			<main className={styles.main}>
+				<UnderDev />
+			</main>
 		</div>
 	);
 }
