db990bbb8422b58039dc81f0ee96b1efc20c291e
mpotulom28 <mpotulom28@gmail.com>
2024-11-08 12:42:51 +0200

    Update layout and metadata; add viewport settings and Google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "./components/Header/page";
 import Footer from "./components/Footer/page";
 import { GlobalProvider } from "./context/GlobalContext";
+// import Head from "next/head";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -21,8 +22,12 @@ export const metadata: Metadata = {
 	description: "Welcome to my Portfolio, Interactive and Responsive Full Stack Web Developer",
 	authors: [{ name: "Manelisi Mpotulo" }],
 	keywords: "Manelisi Mpotulo, portfolio, web developer, software engineer",
-	viewport: "width=device-width, initial-scale=1",
 	robots: "index, follow",
+};
+
+export const viewport = {
+	width: "device-width",
+	initialScale: 1,
 };
 
 export default function RootLayout({
@@ -32,6 +37,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<meta
+				name="google-site-verification"
+				content="7j_MuCkA4qssfr-tjGTpq6LY5pLQQY4pDVtb3b6u4UI"
+			/>
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				<GlobalProvider>
 					<Header />
